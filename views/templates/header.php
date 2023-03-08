220d92ed0eadb45e2b5e7d0c1f4abea99798e9de
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-08 15:24:35 +0100

    escape values

--- views/templates/header.php
+++ views/templates/header.php
@@ -2,7 +2,7 @@
     <ul class="nav nav-tabs" id="myTab" role="tablist">
         <?php foreach ( $this->modules as $module ) : ?>
             <li class="nav-item" role="presentation">
-                <button class="nav-link <?php echo $module === current( $this->modules ) ? 'active' : ''; ?>" id="<?php echo $module['id']; ?>" data-bs-toggle="tab" data-bs-target="#<?php echo $module['pane'] ?>" type="button" role="tab" aria-controls="<?php echo $module['pane']; ?>" aria-selected="<?php echo $module === current( $this->modules ) ? 'true' : 'false'; ?>"><?php echo $module['name']; ?></button>
+                <button class="nav-link <?php echo $module === current( $this->modules ) ? 'active' : ''; ?>" id="<?php echo esc_attr( $module['id'] ); ?>" data-bs-toggle="tab" data-bs-target="#<?php echo esc_attr( $module['pane'] ); ?>" type="button" role="tab" aria-controls="<?php echo esc_attr( $module['pane'] ); ?>" aria-selected="<?php echo $module === current( $this->modules ) ? 'true' : 'false'; ?>"><?php echo esc_html( $module['name'] ); ?></button>
             </li>
         <?php endforeach; ?>
     </ul>
