fb00d9894e621c5708122a10618d8c958ce2624a
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-09 15:01:45 +0100

    Update templates

--- views/templates/header.php
+++ views/templates/header.php
@@ -2,7 +2,9 @@
     <ul class="nav nav-tabs" id="myTab" role="tablist">
         <?php foreach ( $this->modules as $module ) : ?>
             <li class="nav-item" role="presentation">
-                <button class="nav-link <?php echo $module === current( $this->modules ) ? 'active' : ''; ?>" id="<?php echo esc_attr( $module['id'] ); ?>" data-bs-toggle="tab" data-bs-target="#<?php echo esc_attr( $module['pane'] ); ?>" type="button" role="tab" aria-controls="<?php echo esc_attr( $module['pane'] ); ?>" aria-selected="<?php echo $module === current( $this->modules ) ? 'true' : 'false'; ?>"><?php echo esc_html( $module['name'] ); ?></button>
+                <button class="nav-link <?php echo $module === current( $this->modules ) ? 'active' : ''; ?>" id="<?php echo esc_attr( $module['id'] ); ?>_tab" data-bs-toggle="tab" data-bs-target="#<?php echo esc_attr( $module['pane'] ); ?>" type="button" role="tab" aria-controls="<?php echo esc_attr( $module['pane'] ); ?>" aria-selected="<?php echo $module === current( $this->modules ) ? 'true' : 'false'; ?>"><?php echo esc_html( $module['name'] ); ?></button>
             </li>
         <?php endforeach; ?>
     </ul>
+
+    <div class="tab-content" id="myTabContent">
