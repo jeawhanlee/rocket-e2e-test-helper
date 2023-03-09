76a141e4f5d231b759f9ebb499d25962003a5a5f
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-09 15:01:23 +0100

    Call view module loader

--- views/main.php
+++ views/main.php
@@ -1,7 +1,7 @@
 <?php require_once 'templates/header.php'; ?>
-    <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">...</div>
-        <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
-        <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">...</div>
-    </div>
+        <?php foreach ( $this->modules as $module ) : ?>
+            <div class="tab-pane fade<?php echo $module === current( $this->modules ) ? ' show active' : ''; ?>" id="<?php echo esc_attr( $module['pane'] ); ?>" role="tabpanel" aria-labelledby="<?php echo esc_attr( $module['id'] ); ?>_tab" tabindex="0">
+                <?php $this->load_module( $module['id'] ); ?>
+            </div>
+        <?php endforeach ?>
 <?php require_once 'templates/footer.php'; ?>
