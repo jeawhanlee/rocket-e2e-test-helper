9fc5bc78ef3a908cd1e34c1792d322ee12deb797
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-09 18:31:38 +0100

    Added custom style

--- config/assets.php
+++ config/assets.php
@@ -6,6 +6,7 @@ return [
      */
     'styles' => [
         'bootstrap_style' => 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
+        'custom_style' => CONFIG[ 'PLUGIN_URL' ] . 'assets/css/style.css',
     ],
     /**
      * Scripts to import.
