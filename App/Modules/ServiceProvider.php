1f902ba43762cdb1ef234261e789fb9721743fac
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-20 20:53:41 +0100

    Added new module service provider

---
+++ App/Modules/ServiceProvider.php
@@ -0,0 +1,32 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace WP_Rocket_e2e\App\Modules;
+
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+use WP_Rocket_e2e\App\Modules\Cache\Cache;
+use WP_Rocket_e2e\App\Modules\Cache\Subscriber as CacheSubscriber;
+
+
+class ServiceProvider extends AbstractServiceProvider {
+
+    public function provides( string $id ) : bool
+    {
+        $services = [
+            'cache',
+            'cache_subscriber',
+        ];
+        
+        return in_array( $id, $services );
+    }
+
+    public function register() : void
+    {
+        $this->getContainer()->add( 'cache', Cache::class );
+
+        $this->getContainer()->add( 'cache_subscriber', CacheSubscriber::class )
+        ->addArgument( $this->getContainer()->get( 'cache' ) );
+    }
+}
