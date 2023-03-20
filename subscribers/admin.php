52ed42114b99cfe20ea74ae3b2b366bc3c4518d0
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-20 20:50:00 +0100

    Updated subscriber name

--- subscribers/admin.php
+++ subscribers/admin.php
@@ -3,5 +3,5 @@
 use WP_Rocket_e2e\Subscriber;
 
 Subscriber::register( 'assets_subscriber' );
-Subscriber::register( 'template_subscriber' );
+Subscriber::register( 'admin_subscriber' );
 Subscriber::register( 'cache_subscriber' );
