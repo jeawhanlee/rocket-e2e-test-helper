29b3548acd6d7d386029cdedf186a3b92c1f9c8c
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-23 23:13:13 +0100

    Updated view to accomodate notes

--- views/modules/cache.php
+++ views/modules/cache.php
@@ -6,6 +6,11 @@
       <div class="card-body">
         <div class="card-subtitle-box">
           <h6 class="card-subtitle mb-2"><?php echo esc_html( $test_case['name'] ); ?></h6>
+          <?php if ( ! empty( $test_case['note'] ) ) : ?>
+                  <p class="font-sm text-<?php echo esc_attr( $test_case['note']['type'] ); ?>">
+                    <strong><?php echo esc_html( $test_case['note']['text'] );  ?></strong>
+                  </p>
+          <?php endif; ?>
         </div>
         <small class="d-inline-flex px-2 py-1 fw-semibold text-success-emphasis bg-primary-subtle border border-primary-subtle rounded-2" id="<?php echo esc_attr( $test_case_id ) ?>">
             <?php echo $test_case['result'] ? 'Returned True' : 'Returned False';  ?>
