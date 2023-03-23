64ccab130a6ee09833b35197fec60974446746fd
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-23 21:42:57 +0100

    Fix uneven height

--- views/modules/cache.php
+++ views/modules/cache.php
@@ -4,7 +4,9 @@
     <div class="col-sm-3 mb-3 mb-sm-0">
     <div class="card">
       <div class="card-body">
-        <h6 class="card-subtitle mb-2"><?php echo esc_html( $test_case['name'] ); ?></h6>
+        <div class="card-subtitle-box">
+          <h6 class="card-subtitle mb-2"><?php echo esc_html( $test_case['name'] ); ?></h6>
+        </div>
         <small class="d-inline-flex px-2 py-1 fw-semibold text-success-emphasis bg-primary-subtle border border-primary-subtle rounded-2" id="<?php echo esc_attr( $test_case_id ) ?>">
             <?php echo $test_case['result'] ? 'Returned True' : 'Returned False';  ?>
         </small>
