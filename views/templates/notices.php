34270450f92e8def63f282a06e6a07724295c8d9
Michael Lee <michaelleemichaellee408@gmail.com>
2023-03-27 19:25:36 +0100

    Created notices template

---
+++ views/templates/notices.php
@@ -0,0 +1,10 @@
+<?php
+    if ( ! isset( $data ) ) {
+        return;
+    }
+?>
+<div class="notice notice-<?php echo esc_attr( $data['status'] ); ?>">
+    <p id="<?php echo $data['id'] ?? esc_attr( $data['id'] ); ?>">
+        <?php echo esc_html( $data['message'] ); ?>
+    </p>
+</div>
