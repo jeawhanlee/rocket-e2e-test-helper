9ca170815b9cb063f0af3d0f251f841aa5382806
Michael Lee <michaelleemichaellee408@gmail.com>
2023-04-13 13:56:57 +0100

    Return custom filter value

--- App/Modules/Cache/Subscriber.php
+++ App/Modules/Cache/Subscriber.php
@@ -32,6 +32,40 @@ class Subscriber implements Subscriber_Interface {
 	 * @return array
 	 */
 	public static function get_subscribed_events() : array {
-		return [];
+		return [
+            'rocket_post_purge_urls' => 'purge_urls',
+        ];
 	}
+
+    /**
+     * Purge urls.
+     *
+     * @param array $purge_urls urls to purge.
+     * @return mixed
+     */
+    public function purge_urls( array $purge_urls ) {
+        if ( ! __get_option( 'rocket_post_purge_urls' ) ) {
+            return $purge_urls;
+        }
+
+        $rocket_post_purge_urls = __get_option( 'rocket_post_purge_urls' );
+        switch ( $rocket_post_purge_urls ) {
+            case 'false_return':
+                return false;
+            case 'null_return':
+                return null;
+            case 'zero':
+                return 0;
+            case 'empty_string':
+                return '';
+            case 'float':
+                return 2.5;
+            case 'int':
+                return 15;
+            case 'invalid_array':
+                return ['yy',0,True];
+            default:
+                return $purge_urls;
+        }
+    }
 }
